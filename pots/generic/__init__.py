36aa98e164d7fd2245d3f8995929fa0dc9eab8e7
Paul156551 <pavel.ryzhkov.99@mail.ru>
2023-11-21 23:30:23 +0300

    Закончена работа над последним протоколом generic.

---
+++ pots/generic/__init__.py
@@ -0,0 +1,4 @@
+__author__ = 'povilas1565'
+__version__ = '1.0.0'
+__desc__ = 'Generic honeypot that can be bound to configurable port returning configurable or random response'
+from . import genericpot
